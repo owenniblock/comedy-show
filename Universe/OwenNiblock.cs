01afad158d633719b21d84e42e9cf295459cc35c
Owen Niblock <owen.niblock@kumquatcomputing.co.uk>
2018-06-30 21:19:43 +0100

    Missed a bit

--- Universe/OwenNiblock.cs
+++ Universe/OwenNiblock.cs
@@ -6,6 +6,9 @@ namespace Universe
     {
         public bool AmNotARealMan { get; set; }
         public bool ReallyWantATattoo { get; set; }
+        public bool HaveStartedSayingDadPhrases { get; set; }
+        public bool Drive { get; set; }
+        public int ReasonsWhyIamNotARealManCount { get; set; }
 
         public void SlamHeckler()
         {
@@ -63,6 +66,31 @@ namespace Universe
         }
 
         public void Ask(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Feel(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HadAnArgumentWith(Person myWife)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Was(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Collect(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AmNotARealManBecause(string v)
         {
             throw new NotImplementedException();
         }

f0b3d0b76078bdea62d5d36d861362f7a95a8c85
Owen Niblock <ownibloc@microsoft.com>
2020-06-14 21:09:05 +0100

    Adds the incredible Duncan Oakley to the repo

---
+++ the-world/awesome-people/duncan-oakley.js
@@ -0,0 +1,9 @@
+class DuncanOakley {
+    constructor () {
+        this.typeOfPerson = "Awesome"; //TODO: Make enums...!
+        this.music = ["funny", "punky", "guitar-based"];
+        this.website = "https://duncanoakley.bandcamp.com/";
+        this.whereToBuy = ["Bandcamp"];
+        this.whereToSee = ["Live"]; // When life is better
+    }
+}
